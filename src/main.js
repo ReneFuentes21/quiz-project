3f62caad488758e0aee351e5217f15eae0f32594
ili <ilianagranados.05@gmail.com>
2025-05-30 00:27:07 -0600

    cambios en referencias de archivos

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './styles.css';
 import { getQuestions, respuesta, agregarBotonEnviar  } from './question.js';
 //import preguntasData from './data/questions.json';
 
